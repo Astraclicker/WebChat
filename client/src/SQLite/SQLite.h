6ddec83f2d6d00359f7baa3114df133b0d2933c7
my <27442649842qq.com>
2026-09-21 18:58:33 +0800

    加入了用户名和密码插入 用户名和密码修改 查询 上述操作之前会尝试 创建 SQLitepp 全局变量inst 为数据库 然后会创建表名 kTable=user 表名 若存在返回 inst这个数据库 没法验证 cmake在我这里qt环境有问题 Could not find WINDEPLOYQT_EXE using the following names: windeployqt 找不到 windeployqt

--- client/src/SQLite/SQLite.h
+++ client/src/SQLite/SQLite.h
@@ -1,3 +1,7 @@
 #pragma once
+#include <SQLite++/SQLitepp.h>   // 提供 nlohmann::json
+#include <string>
 
-void testFunc();
+void testFunc1(const std::string &user, const std::string &pass);
+void testFunc2(const std::string &user, const std::string &pass);
+nlohmann::json testFunc3(const std::string &user);
